21b8da5a8ac30067967765d0905296d554cf1ec4
Adam Cottrill <adam.cottrill@ontario.ca>
2013-09-06 13:26:36 -0400

    Added view for annual stocking by proponent.
    Also added files for working with virtualenv in emacs (especially on
    windows).

--- fsis2/views.py
+++ fsis2/views.py
@@ -1,22 +1,21 @@
 #from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
 #from django.core.context_processors import csrf
-from django.http import HttpResponseRedirect
 #from django.http import HttpResponse
-#from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import get_object_or_404
 #from django.template import RequestContext
-from django.utils.decorators import method_decorator
+
 from django.core.urlresolvers import reverse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.dates import YearArchiveView
 from django.views.generic import CreateView, UpdateView
+from django.db.models import Sum
+
 
 from olwidget.widgets import InfoMap
 
-from .models import (Event, Lot, TaggingEvent, CWTs_Applied, StockingSite)
-
-import pdb
+from .models import (Event, Lot, TaggingEvent, CWTs_Applied, StockingSite,
+                     Proponent, Species, Strain)
 
 
 def get_map(event_points):
@@ -57,12 +56,6 @@ class EventDetailView(DetailView):
 
         return context
 
-    #@method_decorator(login_required)
-    #def dispatch(self, *args, **kwargs):
-    #    return super(EventDetailView, self).dispatch(*args, **kwargs)
-
-    #event_detail = EventDetailView.as_view()
-
 
 class EventYearArchiveView(YearArchiveView):
     queryset = Event.objects.all()
@@ -70,24 +63,17 @@ class EventYearArchiveView(YearArchiveView):
     make_object_list = True
     allow_future = True
 
-
-
 class EventListView(ListView):
     '''render a list of events optionally filtered by year or lot'''
     queryset = Event.objects.all()
     template_name = "EventList.html"
     paginate_by = 20
 
-    ## @method_decorator(login_required)
-    ## def dispatch(self, *args, **kwargs):
-    ##     return super(EventList, self).dispatch(*args, **kwargs)
-    ##
     def get_context_data(self, **kwargs):
         context = super(EventListView, self).get_context_data(**kwargs)
         cwt = self.kwargs.get('cwt',None)
         context['cwt'] = cwt
 
-        #import pdb; pdb.set_trace()
         #if this is cwt view, we want to include a map
         if cwt:
             events = kwargs.get('object_list')
@@ -96,30 +82,19 @@ class EventListView(ListView):
 
             context['map'] = map
 
-
-
-        #context['year'] = self.kwargs.get('year',None)
         return context
 
     def get_queryset(self):
         self.cwt = self.kwargs.get('cwt',None)
-        #self.tag = self.kwargs.get('year',None)
         if self.cwt:
             #get the list of tagging events associated with that tag number
             te = TaggingEvent.objects.filter(
                     cwts_applied__cwt=self.cwt).values_list('stocking_event')
             #filter the stocking events to those in te
             queryset = Event.objects.filter(id__in=te)
-        #elif self.year:
-            #queryset = Event.objects.filter(year=self.year)
         else:
             queryset = Event.objects.all()
         return queryset
-
-
-    #event_list = EventList.as_view()
-#events_in_year = EventList.as_view()
-#event_by_lot_list = EventList.as_view()
 
 
 class EventCreateView(CreateView):
@@ -144,46 +119,6 @@ class SiteUpdateView(UpdateView):
     model = StockingSite
 
 
-
-
-#@login_required
-#def edit_event(request, event_id):
-#    event = get_object_or_404(Event, pk=event_id)
-#    if request.user.id != event.author.id:
-#        return HttpResponseRedirect(event.get_absolute_url())
-#    if request.method == "POST":
-#        form = EventForm(author=request.user, instance=event,
-#                           data=request.POST)
-#        if form.is_valid():
-#            form.save()
-#            tags = form.cleaned_data['tags']
-#            event.tags.add(*tags)
-#            return HttpResponseRedirect(event.get_absolute_url())
-#    else:
-#        form = EventForm(author=request.user, instance=event)
-#    return render_to_response("events/event_form.html",
-#                            {'form':form, 'add':False},
-#                            context_instance = RequestContext(request))
-#
-#@login_required
-#def add_event(request):
-#    if request.method =="POST":
-#        form = EventForm(author=request.user, data=request.POST)
-#        if form.is_valid():
-#            new_event = form.save(commit=False)
-#            new_event.author=request.user
-#            new_event.save()
-#            tags = form.cleaned_data['tags']
-#            new_event.tags.add(*tags)
-#            return HttpResponseRedirect(new_event.get_absolute_url())
-#    else:
-#        form = EventForm(author=request.user)
-#    return render_to_response('events/event_form.html',
-#                            {"form":form, 'add':True},
-#                            context_instance = RequestContext(request))
-#
-
-
 #==================
 #   LOTS
 
@@ -193,12 +128,6 @@ class LotListView(ListView):
                         event__pk__isnull=False).distinct()
     template_name = "LotList.html"
     paginate_by = 20
-    ##@method_decorator(login_required)
-    ##def dispatch(self, *args, **kwargs):
-    ##    return super(LotList, self).dispatch(*args, **kwargs)
-
-    #lot_list = LotList.as_view()
-
 
 
 class LotDetailView(DetailView):
@@ -209,71 +138,14 @@ class LotDetailView(DetailView):
         lot = kwargs.get('object')
         event_points = lot.get_event_points()
         map = get_map(event_points)
-        #if len(event_points)>0:
-        #    points = [[x[1],x[0]] for x in event_points]
-        #    map = InfoMap(
-        #        points,
-        #        options = {'default_lat': 45,
-        #                   'default_lon': -81.7,
-        #                   'default_zoom':7,
-        #                   'map_div_style': {'width': '700px',
-        #                                     'height': '600px'},
-        #                   }
-        #                )
-        #else:
-        #    map=None
         context['map'] = map
         return context
 
 
-###============================
-### LOT FORMS
-##@login_required
-##def edit_lot(request, slug):
-##
-##    lot = get_object_or_404(Lot, slug=slug)
-##    if request.method == "POST":
-##        form = LotForm(data=request.POST)
-##        if form.is_valid():
-##            form = LotForm(instance=lot,
-##                           data=request.POST)
-##            form.save()
-##            return HttpResponseRedirect(lot.get_absolute_url())
-##    else:
-##        form = LotForm(instance=lot)
-##    return render_to_response("events/lot_form.html",
-##                            {'form':form, 'add':False},
-##                            context_instance = RequestContext(request))
-##
-##@login_required
-##def add_lot(request):
-##    if request.method =="POST":
-##        form = LotForm(data=request.POST)
-##        if form.is_valid():
-##            cd = form.cleaned_data
-##            new_lot = Lot(name = cd['name'],
-##                                    lot_code = cd['lot_code'],
-##                                    mime_type = cd['mime_type'])
-##            new_lot.save()
-##            return HttpResponseRedirect(new_lot.get_absolute_url())
-##    else:
-##        form = LotForm()
-##    return render_to_response('events/lot_form.html',
-##                            {"form":form, 'add':True},
-##                            context_instance = RequestContext(request))
-
-
-
 class CwtListView(ListView):
 
-    #queryset = CWTs_Applied.objects.values('cwt').order_by('cwt').distinct()
     template_name='fsis2/cwt_list.html'
     paginate_by = 20
-    ##@method_decorator(login_required)
-    ##def dispatch(self, *args, **kwargs):
-    ##    return super(CwtListView, self).dispatch(*args, **kwargs)
-
-
 
     def get_queryset(self):
         # Fetch the queryset from the parent get_queryset
@@ -286,7 +158,6 @@ class CwtListView(ListView):
             cwt = cwt.replace('-','')
             return queryset.filter(cwt__icontains=cwt)
         return queryset
-
 
 
 class SiteListView(ListView):
@@ -311,11 +182,30 @@ class SiteDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(SiteDetailView, self).get_context_data(**kwargs)
-
-        #import pdb; pdb.set_trace()
         site = kwargs.get('object')
         site_point = [[site.fsis_site_id, site.geom]]
         map = get_map(site_point)
         context['map'] = map
+        return context
 
+
+class AnnualTotalSpcView(ListView):
+    template_name = "fsis2/annual_total_stkcnt_list.html"
+
+    def get_queryset(self):
+        self.spc = self.kwargs.get('spc', None)
+        queryset = Proponent.objects.filter(
+            lot__species__species_code=self.spc).values('proponent_name',
+            'lot__event__prj_cd').annotate(total=Sum('lot__event__stkcnt'))
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(AnnualTotalSpcView, self).get_context_data(**kwargs)
+        #import pdb; pdb.set_trace()
+
+        spc = self.kwargs.get('spc', None)
+        context['species'] = get_object_or_404(Species, species_code=spc)
+        context['species_list'] = Species.objects.all()
+        context['strain_list'] = Strain.objects.filter(
+            species__species_code=81).values('strain_name').distinct()
         return context
