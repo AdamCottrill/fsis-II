366f7f6f5b3172d01d9465baa31f97ee18f961a1
Adam Cottrill <adam.cottrill@ontario.ca>
2013-10-02 09:31:57 -0400

    Small changes to settings to allow runserver to work in production mode.
    Also removed extra horizontal lines from bottom of detail views.

--- main/settings/production.py
+++ main/settings/production.py
@@ -6,7 +6,6 @@ TEMPLATE_DEBUG = DEBUG
 #these need to be reset to work with heroku:
 DATABASES = {
     'default': {
-    'default': {
          'ENGINE': 'django.contrib.gis.db.backends.postgis',
          'NAME': 'fsis2',
  ***REMOVED***
