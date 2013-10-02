74879265c663e93d1bbaea696fb4f08cce29d906
Adam Cottrill <adam.cottrill@ontario.ca>
2013-10-02 09:31:57 -0400

    Small changes to settings to allow runserver to work in production mode.
    Also removed extra horizontal lines from bottom of detail views.

--- main/settings/base.py
+++ main/settings/base.py
@@ -34,7 +34,8 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+# not supposed to use * in production, but nothing else seems to work:
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -77,6 +78,7 @@ ADMIN_MEDIA_PREFIX = '/admin/media/'
 # Example: "/var/www/example.com/static/"
 #STATIC_ROOT = ''
 STATIC_ROOT = root("static_root/")
+
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
