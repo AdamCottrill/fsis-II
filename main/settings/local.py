69109f7677df9b330c51b3bc5cffe9d984e3062a
Adam Cottrill <racottrill@bmts.com>
2013-11-22 08:16:08 -0500

    Small changes to local.py and lot_detail.html to finalize merge.

--- main/settings/local.py
+++ main/settings/local.py
@@ -2,39 +2,16 @@ from main.settings.base import *
 
 DATABASES = {
     'default': {
-<<<<<<< HEAD
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '%s/db/fsis2.db' % root(),
-      }
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'fsis2',
+        'USER': 'adam',
+        'PASSWORD': 'django',
+    }
 
-
-    #'default': {
-    #     'ENGINE': 'django.contrib.gis.db.backends.postgis',
-    #     'NAME': 'fsis2',
-    #     'USER': 'adam',
-    #     'PASSWORD': 'django',
-    #
-    # }
-
-
-#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#          'NAME': 'snippets',
-#          'USER': 'adam',
-#          'Password': 'adam',
-        #  'HOST': '',
-        #  'PORT': '',
-
-=======
-         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-         'NAME': 'fsis2',
-         'USER': 'adam',
-         'PASSWORD': 'django',
-     }
->>>>>>> 4e315ca2b10ddf64238133ee8ed784527acff6bf
 }
 
 
-INTERNAL_IPS = ('127.0.0.1', )   #added for debug toolbar
+INTERNAL_IPS = ('127.0.0.1', )
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
 }
