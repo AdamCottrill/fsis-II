038456ef3e2db3dbfbddee7da8bb2f6212f28025
Adam Cottrill <racottrill@bmts.com>
2013-11-27 08:49:58 -0500

    Finished test of cwt view without stocking events, removed button bars from detail templates.

--- fsis2/tests/integration_tests/views_tests.py
+++ fsis2/tests/integration_tests/views_tests.py
@@ -12,47 +12,6 @@ from fsis2.tests.factories import *
 from cwts.tests.factories import CWT_RecoveryFactory, CWTFactory
 
 
-class TestCWTDetailViews(TestCase):
-    
-    def setUp(self):
-        self.cwt = CWTFactory(cwt='123456')
-        
-    def test_handle_cwt_without_events(self):
-        '''If we try to access a cwt that does not have any stocking events
-        associated with it, we should handle that gracefully.'''
-        
-        response = self.client.get(reverse('cwt_detail',
-                                           kwargs={'cwt_number':
-                                                   self.cwt.cwt}))
-        self.assertEqual(response.status_code, 200)
-
-        #print "response = %s" % response
-
-        cwt = self.cwt.cwt
-        cwt_number = "-".join([cwt[:2], cwt[2:4], cwt[4:]])
-        self.assertContains(response, cwt_number)
-
-        #self.assertContains(response, int(self.cwt.year_class), html=True)
-        #self.assertContains(response, int(self.cwt.stock_year), html=True)
-        #self.assertContains(response, self.cwt.strain, html=True)
-        #self.assertContains(response, self.cwt.get_strain_display(), html=True)
-        self.assertIn(self.cwt.get_strain_display(), response)
-        self.assertContains(response, self.cwt.get_devlopment_stage_display())
-
-        self.assertContains(response, self.cwt.tag_cnt, html=True)
-        self.assertContains(response, self.cwt.clipa)
-        self.assertContains(response, self.cwt.plant_site)
-
-        self.assertContains(response, self.cwt.ltrz, html=True)
-        self.assertContains(response, self.cwt.get_ltrz_display(), html=True)
-
-        self.assertContains(response, self.cwt.agency)
-        self.assertContains(response, self.cwt.hatchery)
-
-
-
-        self.fail("Finish this test")
-
 
 
 class TestLotList(TestCase):
