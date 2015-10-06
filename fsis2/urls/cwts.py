ed1245b111b43d1345c7d2a47a7d5f340564b16d
Adam Cottrill <adam.cottrill@ontario.ca>
2015-10-06 11:49:37 -0400

    Split urls.py
    urls.py was getting unwieldy.  Created urls directory and created
    urlconf files for sites, hatcheries, lots, events, and cwts.  All tests
    continue to pass.

---
+++ fsis2/urls/cwts.py
@@ -0,0 +1,61 @@
+'''
+=============================================================
+c:/1work/Python/djcode/fsis2/fsis2/urls/cwts.py
+Created: 06 Oct 2015 09:24:10
+
+
+DESCRIPTION:
+
+urls associated with cwts
+
+A. Cottrill
+=============================================================
+'''
+
+
+from fsis2.views import (CwtListView,
+                    cwt_detail_view,
+                    cwt_stocked_mu,
+                    cwt_recovered_mu,
+                    ManagementUnitListView,)
+
+
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('',
+
+        url(
+            regex=r'^$',
+            view=CwtListView.as_view(),
+            name='cwt_list'
+            ),
+
+        url(
+            regex=r'detail/(?P<cwt_number>\d{6})$',
+            view=cwt_detail_view,
+            name='cwt_detail'
+            ),
+
+        #recoveries in management unit
+        url(
+            regex=r'stocked_managment_unit/(?P<slug>[-_A-Za-z0-9]+)$',
+            view=cwt_stocked_mu,
+            name='cwt_stocked_mu'
+            ),
+
+        #recoveries of cwt stocked in cwt
+        url(
+            regex=r'recovered_managment_unit/(?P<slug>[-_A-Za-z0-9]+)$',
+            view=cwt_recovered_mu,
+            name='cwt_recovered_mu'
+            ),
+
+        #================
+        #Managment Unit list
+        url(
+            regex=r'^management_units/$',
+            view=ManagementUnitListView.as_view(),
+            name='mu_list'
+            ),
+
+)
---
+++ fsis2/urls/sites.py
@@ -0,0 +1,61 @@
+'''
+=============================================================
+c:/1work/Python/djcode/fsis2/fsis2/urls/sites.py
+Created: 06 Oct 2015 09:28:42
+
+
+DESCRIPTION:
+
+urls associated with stocking sites
+
+A. Cottrill
+=============================================================
+'''
+
+
+from django.conf.urls import patterns, url
+
+from fsis2.views import (SiteDetailView,
+                         SiteListView,
+                         SiteCreateView,
+                         SiteUpdateView,
+                         find_sites)
+
+
+urlpatterns = patterns('',
+
+        #================
+        #    STOCKING SITEs
+
+        #create stocking site
+        url(
+            regex=r'add$',
+            view=SiteCreateView.as_view(),
+            name='site_create'
+            ),
+
+        #update stocking site
+        url(
+            regex=r'update/(?P<pk>\d+)$',
+            view=SiteUpdateView.as_view(),
+            name='site_update'
+            ),
+
+        #stocking sites
+        url(
+            regex=r'^sites/$',
+            view=SiteListView.as_view(),
+            name='site_list'
+            ),
+
+        #stocking site details
+        url(
+            regex=r'detail/(?P<pk>\d+)$',
+            view=SiteDetailView.as_view(),
+            name='site_detail'
+            ),
+
+        url(regex=r'^find_sites/$',
+            view=find_sites,
+            name='find_sites'),
+)
