efc5512b0b66d647275b06f3aa8ffab86ad9548f
Adam Cottrill <adam.cottrill@ontario.ca>
2016-08-10 11:26:33 -0400

    Removed Openshift code and structure. Added Purge Species

--- bin/run_dev_server.sh
+++ bin/run_dev_server.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
-python manage.py runserver_plus localhost:8090 --settings=main.settings.local
+python manage.py runserver --settings=main.settings.local
--- wsgi/bin/run_dev_server.bat
+++
@@ -1,1 +0,0 @@
-python manage.py runserver --settings=main.settings.local
